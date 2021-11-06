54cbe6bf5b2e19626550835314c46c382ae140e2
David Marquez <grinmor2016@gmail.com>
2021-11-07 00:34:36 +0100

    footer

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -18,7 +18,7 @@ export class GameService {
   ) {}
 
   back(){
-    this.router.navigate(['/login']);
+    this.router.navigate(['/home']);
   }
 
   async checkResult(myBet:Bet, yourBet:Bet): Promise<number>{
