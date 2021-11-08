0389441e00c5bfbc3a4f6b0da4a1db73c16c5dd8
David Marquez <grinmor2016@gmail.com>
2021-11-08 01:04:48 +0100

    server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/PPT'));
+app.use(express.static('dist/PPT'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/PPT/index.html'));
-});
+    res.sendFile(path.join('../dist/PPT/index.html'));
+  });
 
 app.listen(process.env.PORT || 8080);
